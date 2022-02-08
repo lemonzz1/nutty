19454cc0b4dfe73e75c3986a40acc9af37a3222c
lemon_project_2 <hsdnmzz@163.com>
2022-02-08 12:05:57 +0800

    修复bug，优化Scanner类，提高代码的可用性，Scanner类新增故障排除功能

--- src/com/System/console/Scanner.java
+++ src/com/System/console/Scanner.java
@@ -8,55 +8,48 @@ public class Scanner implements Console_Scanner {
 	public static class input_title {
 
 		public static String PassWord(String title) {
-			cont.print(1, title);
-			String input_PassWord = input_class.next();
+			String input_PassWord = input__.PassWord(title);
 			Project.logs("Scanner.input_title.PassWord()[String:PassWord]输入:处于安全考虑，不能打印PassWord");
 			return input_PassWord;
 		}
 
 		public static String String(String title) {
-			cont.print(1, title);
-			String input_text = input_class.next();
-			Project.logs("Scanner.input_title.String()[String]输入:" + input_text);
+			String input_text = input__.String(title);
+			Project.logs("Scanner.input_title.String()[String]输入:" + title + input_text);
 			return input_text;
 		}
 
 		public static int Int(String title) {
-			cont.print(1, title);
-			int input_text = input_class.nextInt();
-			Project.logs("Scanner.input_title.Int()[int]输入:" + input_text);
+			int input_text = input__.Int(title);
+			Project.logs("Scanner.input_title.Int()[int]输入:" + title + input_text);
 			return input_text;
 		}
 
 
 		public static boolean Boolean(String title) {
-			cont.print(1, title);
-			boolean input_text = input_class.hasNext();
-			Project.logs("Scanner.input_title.Boolean()[boolean]输入:" + input_text);
+			boolean input_text = input__.Boolean(title);
+			Project.logs("Scanner.input_title.Boolean()[boolean]输入:" + title + input_text);
 			return input_text;
 		}
 
 
 		public static double Double(String title) {
-			cont.print(1, title);
-			double input_text = input_class.nextDouble();
-			Project.logs("Scanner.input_title.Double()[double]输入:" + input_text);
+			double input_text = input__.Double(title);
+			Project.logs("Scanner.input_title.Double()[double]输入:" + title + input_text);
 			return input_text;
 		}
 
 
 		public static byte Byte(String title) {
-			cont.print(1, title);
-			byte input_text = input_class.nextByte();
-			Project.logs("Scanner.input_title.Byte()[byte]输入:" + input_text);
+			byte input_text = input__.Byte(title);
+			Project.logs("Scanner.input_title.Byte()[byte]输入:" + title + input_text);
 			return input_text;
 		}
 
 
 		public static float Float(String title) {
-			cont.print(1, title);
-			float input_text = input_class.nextFloat();
-			Project.logs("Scanner.input_title.Float()[float]输入:" + input_text);
+			float input_text = input__.Float(title);
+			Project.logs("Scanner.input_title.Float()[float]输入:" + title + input_text);
 			return input_text;
 		}
 	}
@@ -64,45 +57,242 @@ public class Scanner implements Console_Scanner {
 	public static class input {
 
 		public static String PassWord() {
-			String input_PassWord = input_class.next();
+			String input_PassWord = input__.PassWord();
 			Project.logs("Scanner.input.PassWord()[String:PassWord]输入:处于安全考虑，不能打印PassWord");
 			return input_PassWord;
 		}
 
 		public static String String() {
-			String input_text = input_class.next();
+			String input_text = input__.String();
 			Project.logs("Scanner.input.String()[String]输入:" + input_text);
 			return input_text;
 		}
 
 		public static int Int() {
-			int input_text = input_class.nextInt();
+			int input_text = input__.Int();
 			Project.logs("Scanner.input.Int()[int]输入:" + input_text);
 			return input_text;
 		}
 
 		public static boolean Boolean() {
-			boolean input_text = input_class.hasNext();
+			boolean input_text = input__.Boolean();
 			Project.logs("Scanner.input.Boolean()[boolean]输入:" + input_text);
 			return input_text;
 		}
 
 		public static double Double() {
-			double input_text = input_class.nextDouble();
+			double input_text = input__.Double();
 			Project.logs("Scanner.input.Double()[double]输入:" + input_text);
 			return input_text;
 		}
 
 		public static byte Byte() {
-			byte input_text = input_class.nextByte();
+			byte input_text = input__.Byte();
 			Project.logs("Scanner.input.Byte()[byte]输入:" + input_text);
 			return input_text;
 		}
 
 		public static float Float() {
-			float input_text = input_class.nextFloat();
+			float input_text = input__.Float();
 			Project.logs("Scanner.input.Float()[float]输入:" + input_text);
 			return input_text;
+		}
+
+
+		public static String PassWord(String title) {
+			String input_PassWord = input__.PassWord(title);
+			Project.logs("Scanner.input.PassWord()[String:PassWord]输入:处于安全考虑，不能打印PassWord");
+			return input_PassWord;
+		}
+
+		public static String String(String title) {
+			String input_text = input__.String(title);
+			Project.logs("Scanner.input.String()[String]输入:" + title + input_text);
+			return input_text;
+		}
+
+		public static int Int(String title) {
+			int input_text = input__.Int(title);
+			Project.logs("Scanner.input.Int()[int]输入:" + title + input_text);
+			return input_text;
+		}
+
+
+		public static boolean Boolean(String title) {
+			boolean input_text = input__.Boolean(title);
+			Project.logs("Scanner.input.Boolean()[boolean]输入:" + title + input_text);
+			return input_text;
+		}
+
+
+		public static double Double(String title) {
+			double input_text = input__.Double(title);
+			Project.logs("Scanner.input.Double()[double]输入:" + title + input_text);
+			return input_text;
+		}
+
+
+		public static byte Byte(String title) {
+			byte input_text = input__.Byte(title);
+			Project.logs("Scanner.input.Byte()[byte]输入:" + title + input_text);
+			return input_text;
+		}
+
+
+		public static float Float(String title) {
+			float input_text = input__.Float(title);
+			Project.logs("Scanner.input.Float()[float]输入:" + title + input_text);
+			return input_text;
+		}
+	}
+
+	private static class input__ {
+		public static String PassWord(String title) {
+			try {
+				cont.print(1, title);
+				return input_class.next();
+			} catch (Exception e) {
+				cont.print(4, "input__.PassWorld(" + title + ")遇到致命错误");
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		public static String String(String title) {
+			try {
+				cont.print(1, title);
+				return input_class.next();
+			} catch (Exception e) {
+				cont.print(4, "input__.String(" + title + ")遇到致命错误");
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		public static int Int(String title) {
+			try {
+				cont.print(1, title);
+				return input_class.nextInt();
+			} catch (Exception e) {
+				cont.print(4, "input__.Int(" + title + ")遇到致命错误");
+				e.printStackTrace();
+			}
+			return 0;
+		}
+
+		public static boolean Boolean(String title) {
+			try {
+				cont.print(1, title);
+				return input_class.hasNext();
+			} catch (Exception e) {
+				cont.print(4, "input__.Boolean(" + title + ")遇到致命错误");
+				e.printStackTrace();
+			}
+			return false;
+		}
+
+		public static double Double(String title) {
+			try {
+				cont.print(1, title);
+				return input_class.nextDouble();
+			} catch (Exception e) {
+				cont.print(4, "input__.Double(" + title + ")遇到致命错误");
+				e.printStackTrace();
+			}
+			return 0;
+		}
+
+		public static byte Byte(String title) {
+			try {
+				cont.print(1, title);
+				return input_class.nextByte();
+			} catch (Exception e) {
+				cont.print(4, "input__.Byte(" + title + ")遇到致命错误");
+				e.printStackTrace();
+			}
+			return 0;
+
+		}
+
+		public static float Float(String title) {
+			try {
+				cont.print(1, title);
+				return input_class.nextFloat();
+			} catch (Exception e) {
+				cont.print(4, "input__.Float(" + title + ")遇到致命错误");
+				e.printStackTrace();
+			}
+			return 0;
+		}
+
+		public static String PassWord() {
+			try {
+				return input_class.next();
+			} catch (Exception e) {
+				cont.print(4, "input__.PassWorld()遇到致命错误");
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		public static String String() {
+			try {
+				return input_class.next();
+			} catch (Exception e) {
+				cont.print(4, "input__.String()遇到致命错误");
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		public static int Int() {
+			try {
+				return input_class.nextInt();
+			} catch (Exception e) {
+				cont.print(4, "input__.Int()遇到致命错误");
+				e.printStackTrace();
+			}
+			return 0;
+		}
+
+		public static boolean Boolean() {
+			try {
+				return input_class.hasNext();
+			} catch (Exception e) {
+				cont.print(4, "input__.Boolean()遇到致命错误");
+				e.printStackTrace();
+			}
+			return false;
+		}
+
+		public static double Double() {
+			try {
+				return input_class.nextDouble();
+			} catch (Exception e) {
+				cont.print(4, "input__.Double()遇到致命错误");
+				e.printStackTrace();
+			}
+			return 0;
+		}
+
+		public static byte Byte() {
+			try {
+				return input_class.nextByte();
+			} catch (Exception e) {
+				cont.print(4, "input__.Byte()遇到致命错误");
+				e.printStackTrace();
+			}
+			return 0;
+		}
+
+		public static float Float() {
+			try {
+				return input_class.nextFloat();
+			} catch (Exception e) {
+				cont.print(4, "input__.Float()遇到致命错误");
+				e.printStackTrace();
+			}
+			return 0;
 		}
 	}
 
