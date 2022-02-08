f72ea078f9e13e37858dd9e6328b1745ededdf6e
lemon_project_2 <hsdnmzz@163.com>
2022-02-08 16:16:15 +0800

    提交更新

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+import com.System.print.cont;
+import com.variable.SDT;
+
+public class Test {
+	public static void main(String[] args) {
+		int i = SDT.String_int("123456");
+		cont.print(2, i);
+	}
+}
